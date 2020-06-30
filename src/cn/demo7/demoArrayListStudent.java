83a42edc9146f38ceb990bba1ca22148a76e6868
Lic <87831498@qq.com>
2020-06-30 15:15:28 +0800

    1

---
+++ src/cn/demo7/demoArrayListStudent.java
@@ -0,0 +1,28 @@
+package cn.demo7;
+
+import java.util.ArrayList;
+
+public class demoArrayListStudent {
+    /*
+    自定义4个学生对象，添加集合，遍历
+
+     */
+
+    public static void main(String[] args) {
+        ArrayList<Student> list=new ArrayList<>();
+        Student stu1=new Student("a",11);
+        Student stu2=new Student("b",22);
+        Student stu3=new Student("c",23);
+        Student stu4=new Student("d",34);
+        Student stu5=new Student("e",32);
+        list.add(stu1);
+        list.add(stu2);
+        list.add(stu3);
+        list.add(stu4);
+        list.add(stu5);
+        for (int i = 0; i < list.size(); i++) {
+            Student stu=list.get(i);
+            System.out.println("name:"+stu.getName()+"  ,age:"+stu.getAge());
+        }
+    }
+}
