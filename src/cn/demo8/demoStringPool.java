d7b295c856e11bbb838d45eadb96b8eb83559ae8
Lic <87831498@qq.com>
2020-06-30 15:50:34 +0800

    commit

---
+++ src/cn/demo8/demoStringPool.java
@@ -0,0 +1,18 @@
+package cn.demo8;
+/*
+字符串常量池：直接用双引号的字符串，就在其中。
+
+对于基本型：==是【数值】的比较
+对于引用型:==是进行【地址】的比较
+ */
+public class demoStringPool {
+    public static void main(String[] args) {
+        String str1="abc";
+        String str2="abc";
+        char[] charArray={'a','b','c'};
+        String str3=new String(charArray);
+        System.out.println(str1==str2);
+        System.out.println(str3==str2);
+        System.out.println(str1==str3);
+    }
+}
