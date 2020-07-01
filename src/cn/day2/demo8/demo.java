39143bb2eef7db9476e6444ca8264f1cb681a572
Lic <87831498@qq.com>
2020-07-01 16:23:58 +0800

    commit

---
+++ src/cn/day2/demo8/demo.java
@@ -0,0 +1,11 @@
+package cn.day2.demo8;
+
+public class demo {
+    public static void main(String[] args) {
+        myInterfaceAbstractImpl impl=new myInterfaceAbstractImpl();
+        impl.methodAbs1();
+        impl.methodAbs2();
+        impl.methodAbs3();
+        impl.methodAbs4();
+    }
+}
