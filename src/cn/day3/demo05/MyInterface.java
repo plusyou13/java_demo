a6cb443c55b95d53533f669d30347818828b76dc
Lic <87831498@qq.com>
2020-07-02 14:10:34 +0800

    commit

---
+++ src/cn/day3/demo05/MyInterface.java
@@ -0,0 +1,9 @@
+package cn.day3.demo05;
+
+public interface MyInterface {
+
+    void method1(); // 抽象方法
+
+    void method2();
+
+}
---
+++ src/cn/day3/demo07/SkillImpl.java
@@ -0,0 +1,8 @@
+package cn.day3.demo07;
+
+public class SkillImpl implements Skill {
+    @Override
+    public void use() {
+        System.out.println("Biu~biu~biu~");
+    }
+}
